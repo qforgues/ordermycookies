8d1891bf388b100342014df282ed03b9b5e79c92
Quentin Forgues <quentin.forgues@gmail.com>
2025-05-27 13:09:54 -0400

    email fix 6

--- send_email.php
+++ send_email.php
@@ -2,7 +2,7 @@
 function sendCustomerEmail($to, $subject, $bodyHtml) {
     $headers  = "MIME-Version: 1.0\r\n";
     $headers .= "Content-type: text/html; charset=UTF-8\r\n";
-    $headers .= "From: Courtney's Cookies <courtney@ordermycookies.com>\r\n";
+    $headers .= "From: Courtney's Cookies <orders@ordermycookies.com>\r\n";
 
     return mail($to, $subject, $bodyHtml, $headers);
 }
